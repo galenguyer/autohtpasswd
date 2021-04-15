4339c822a19b084a8321fec1ce5915209ec4ba55
Galen Guyer <galen@galenguyer.com>
2021-04-15 17:14:18 -0400

    Add check for root

--- autohtpasswd.c
+++ autohtpasswd.c
@@ -2,8 +2,13 @@
 #include <stdio.h>
 #include <strings.h>
 #include <shadow.h>
+#include <unistd.h>
 
 int main() {
+    if (getuid() != 0) {
+        puts("Must be run as root.");
+        exit(1);
+    }
     setspent();
     struct spwd* curr = getspent();
     while (curr != NULL) {
